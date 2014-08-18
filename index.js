31e06b402c78471dcea714e8f21bec17f0118893
Mathias Buus <mathiasbuus@gmail.com>
2014-08-19 00:58:22 +0200

    more blob store interface compliant

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var request = require('request')
 var once = require('once')
+var eos = require('end-of-stream')
 
 module.exports = function(remote) {
   if (remote.indexOf('://') === -1) remote = 'http://'+remote
@@ -15,15 +16,21 @@ module.exports = function(remote) {
     })
   }
 
+  var onend = function(req, cb) {
+    eos(req, cb)
+    return req
+  }
+
   that.createReadStream = function(opts) {
     if (opts.link) return request(opts.link)
     if (opts.hash) return request(remote+'/api/blobs/'+opts.hash)
     return req('GET', opts)
   }
 
-  that.createWriteStream = function(opts) {
-    if (opts.link) return request.put(opts.link)
-    return req('POST', opts)
+  that.createWriteStream = function(opts, cb) {
+    if (typeof opts === 'function') throw new Error('options are required')
+    if (opts.link) return onend(request.put(opts.link), cb)
+    return onend(req('POST', opts), cb)
   }
 
   that.exists = function(opts, cb) {
